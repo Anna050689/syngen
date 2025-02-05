cdf3e54dae52edf26b07bbeb31ae9645518905b8
Hanna Imshenetska <69158110+Anna050689@users.noreply.github.com>
2025-02-05 18:08:50 +0200

    Update src/syngen/ml/context/__init__.py

--- src/syngen/ml/context/__init__.py
+++ src/syngen/ml/context/__init__.py
@@ -1,1 +1,6 @@
+"""
+This module provides the Context class and associated functions for managing
+configuration contexts in a singleton pattern.
+"""
+
 from syngen.ml.context.context import Context, global_context, get_context  # noqa: F401
