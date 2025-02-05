34c1650a011e6ca8ad21e8cbcf1948fef45ab781
Hanna Imshenetska <69158110+Anna050689@users.noreply.github.com>
2025-02-05 14:41:44 +0200

    Update src/syngen/ml/context/context.py

--- src/syngen/ml/context/context.py
+++ src/syngen/ml/context/context.py
@@ -3,13 +3,40 @@ import copy
 
 
 class Context:
+    """
+    A class to manage configuration context.
+
+    Attributes:
+    ----------
+    config : dict
+        A dictionary to store configuration settings.
+    """
     def __init__(self):
+        """
+        Initializes a new instance of Context with an empty configuration.
+        """
         self.config = {}
 
     def set_config(self, value: Dict):
+        """
+        Sets the configuration to a deep copy of the provided value.
+
+        Parameters:
+        ----------
+        value : dict
+            The configuration dictionary to set.
+        """
         self.config = copy.deepcopy(value)
 
     def get_config(self) -> Dict:
+        """
+        Retrieves the current configuration.
+
+        Returns:
+        -------
+        dict
+            The current configuration dictionary.
+        """
         return self.config
 
 
@@ -18,6 +45,14 @@ _config_instance: Context = None
 
 
 def get_context() -> Context:
+    """
+    Retrieves the singleton instance of Context.
+
+    Returns:
+    -------
+    Context
+        The singleton instance of Context.
+    """
     global _config_instance
     if _config_instance is None:
         _config_instance = Context()
@@ -25,5 +60,13 @@ def get_context() -> Context:
 
 
 def global_context(metadata: Dict):
+    """
+    Sets the global configuration context with the provided metadata.
+
+    Parameters:
+    ----------
+    metadata : dict
+        The metadata to set as the global configuration.
+    """
     global_config = get_context()
     global_config.set_config(metadata)
