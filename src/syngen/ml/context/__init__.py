d169a04654dd85f596b28735bf1e833891452d8d
Hanna Imshenetska <69158110+Anna050689@users.noreply.github.com>
2025-02-05 14:41:47 +0200

    Update src/syngen/ml/context/__init__.py

--- src/syngen/ml/context/__init__.py
+++ src/syngen/ml/context/__init__.py
@@ -1,1 +1,18 @@
+"""
+This module provides the Context class and related functions for managing configuration context.
+
+Classes:
+--------
+Context
+    A class to manage configuration context.
+
+Functions:
+----------
+get_context()
+    Retrieves the singleton instance of Context.
+
+global_context(metadata: Dict)
+    Sets the global configuration context with the provided metadata.
+"""
+
 from syngen.ml.context.context import Context, global_context, get_context  # noqa: F401
